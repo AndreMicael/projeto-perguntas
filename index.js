ff264992970211cd64ae3f55e5a9aab0b00677ac
AndreMicael <andremicael@gmail.com>
2023-11-15 19:22:34 -0400

    Adicionado o Body Parser

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express"); //Importando express
 const app = express(); //Iniciando o express
+const bodyParser = require("body-parser"); //Importando body-parser
 
 app.set('view engine', 'ejs'); //Setando o ejs como view engine
 app.use(express.static('public')); //Setando a pasta public como estatica
@@ -10,5 +11,9 @@ app.get("/", (req, res) => { //Rota principal
 app.get("/perguntas", (req, res) => { //Rota perguntas
     res.render("perguntar"); //Renderizando a pagina perguntas
 }); 
+
+app.post("/salvar", (req, res) => { //Rota perguntar
+    res.send("Formulario recebido!"); //Renderizando a pagina perguntas
+});
 
 app.listen(4000,()=>{console.log("Servidor rodando!");});
