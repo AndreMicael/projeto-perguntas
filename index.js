4d17906c287f4cf92c07cd2e28ed74318d89794a
AndreMicael <andremicael@gmail.com>
2023-11-15 19:08:22 -0400

    Adicionado o Partials

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ app.set('view engine', 'ejs'); //Setando o ejs como view engine
 app.use(express.static('public')); //Setando a pasta public como estatica
 
 app.get("/", (req, res) => { //Rota principal
-    res.render("principal/index"); //Renderizando a pagina index
+    res.render("index"); //Renderizando a pagina index
 }); 
 app.get("/perguntas", (req, res) => { //Rota perguntas
-    res.render("principal/perguntar"); //Renderizando a pagina perguntas
+    res.render("perguntar"); //Renderizando a pagina perguntas
 }); 
 
 app.listen(4000,()=>{console.log("Servidor rodando!");});
